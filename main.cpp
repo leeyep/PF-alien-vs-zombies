e09159481f7bc1e3d8f66de9d479024acf95e93c
KwangZhengOng <123556351+KwangZhengOng@users.noreply.github.com>
2023-02-06 23:13:04 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,219 +16,196 @@
 // game over/you win
 // video of p2
 
-#include "helper.cpp"
-#include "command.cpp"
 #include <iostream>
-#include <vector>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
-
-// void Gamesetting(int &selection)
-//{
-//    cout << "Your Selection is =";
-//    int temp;
-//    cin >> temp;
-//
-//    const int Numberofrows = 3;
-//    const int NumberofColumns = 5;
-//    const int NumberofZombies = 1;
-//    int Numbofrows;
-//    int NumbofColumns;
-//    int NumbofZombies;
-//    if (temp == 1)
-//        ;
-//    cout << "Numbers of rows:(#only odd numbers)" << endl;
-//    cin >> Numbofrows;
-//    cout << "Number of columns: " << endl;
-//    cin >> NumbofColumns;
-//    cout << "Number of Zombies" << endl;
-//    cin >> NumbofZombies;
-
-//   if (temp == 2)
-//    cout << "Number of rows: " << Numberofrows << endl;
-//    cout << "Number of Columns:" << NumberofColumns << endl;
-//    cout << "Number of Zombies:" << NumberofZombies << endl;
-//} <-----------------------------------------------------------------------(DONT DELETE YET NOT CONFIRMED)
+char Board[50][50];
 
 void display_settingscreen(int &numofrows, int &numofcolumns, int &numofzombies)
 {
-a:
-    cout << "Number of rows (Odd numbers only) : ";
-    cin >> numofrows;
-    if (numofrows < 1 || numofrows > 15)
+    cout << "Do You Wanna Change Your Game Setting? (1=Yes 2 = No) :";
+    int temp;
+    cin >> temp;
+    if (temp == 1)
     {
-        cout << "number is invalid, cannot be less than 1 or more than 15" << endl;
-        goto a;
-    }
+        cout << "Number of rows (Odd numbers only) : ";
+        cin >> numofrows;
+        while (numofrows < 1 || numofrows > 15 || numofrows % 2 != 1)
+        {
+            cout << "Invalid input. Number of rows must be odd and between 1 and 15." << endl;
+            cout << "Number of rows (Odd numbers only) : ";
+            cin >> numofrows;
+        }
 
-b:
-    cout << "Number of columns (Odd numbers only) : ";
-    cin >> numofcolumns;
-    if (numofcolumns < 1 || numofcolumns > 49)
-    {
-        cout << "number is invalid, cannot be less than 1 or more than 39" << endl;
-        goto b;
-    }
+        cout << "Number of columns (Odd numbers only) : ";
+        cin >> numofcolumns;
+        while (numofcolumns < 1 || numofcolumns > 49 || numofcolumns % 2 != 1)
+        {
+            cout << "Invalid input. Number of columns must be odd and between 1 and 49." << endl;
+            cout << "Number of columns (Odd numbers only) : ";
+            cin >> numofcolumns;
+        }
 
-c:
-    cout << "Number of zombies : ";
-    cin >> numofzombies;
-    if (numofzombies < 1 || numofzombies > 10)
-    {
-        cout << "number is invalid, cannot be less than 1 or more than 10" << endl;
-        goto c;
+        cout << "Number of zombies : ";
+        cin >> numofzombies;
+        while (numofzombies < 1 || numofzombies > 10)
+        {
+            cout << "Invalid input. Number of zombies must be between 1 and 10." << endl;
+            cout << "Number of zombies : ";
+            cin >> numofzombies;
+        }
     }
-    pf::Pause();
-    pf::ClearScreen();
+    if (temp == 2)
+    {
+        cout << "Number of rows: " << numofrows << endl;
+        cout << "Number of Columns:" << numofcolumns << endl;
+        cout << "Number of Zombies:" << numofzombies << endl;
+    }
+}
+void createBoard(int row, int column)
+{
+
+    for (int rowcounter = 0; rowcounter < row; rowcounter++)
+    {
+        for (int columncounter = 0; columncounter < column; columncounter++)
+        {
+            cout << "+-";
+        }
+        cout << "+" << endl;
+        for (int columncounter = 0; columncounter < column; columncounter++)
+        {
+            cout << "|"
+                 << Board[rowcounter][columncounter];
+        }
+        cout << "|" << endl;
+    }
+    for (int columncounter = 0; columncounter < column; columncounter++)
+    {
+        cout << "+-";
+    }
+    cout << "+" << endl;
 }
 
-int const row = 30;
-int const column = 30;
-char board[row][column];
-void Createboard(int row, int column)
+void feature(int &numofrows, int &numofcolumns, int &numofzombies)
 {
-    for (int rows = 0; rows < row; rows++)
+    int random;
+    int randomrows;
+    int randomcolumns;
+    srand(time(NULL));
+    for (int rowcounter = 0; rowcounter < numofrows; rowcounter++)
     {
-        for (int columns = 0; columns < column; columns++)
+        for (int columncounter = 0; columncounter < numofcolumns; columncounter++)
         {
-            if (rows % 2 == 0 && columns % 2 == 0)
+            random = rand() % 100;
+            if (random < 20)
             {
-                board[rows][columns] = '+';
+                Board[rowcounter][columncounter] = 'p';
             }
-            else if (rows % 2 == 0)
+            else if (random < 40)
             {
-                board[rows][columns] = '-';
+                Board[rowcounter][columncounter] = 'h';
             }
-            else if (columns % 2 == 0)
+            else if (random < 45)
             {
-                board[rows][columns] = '|';
+                Board[rowcounter][columncounter] = '^';
             }
-            else if (columns % 2 != 0)
+            else if (random < 50)
             {
-                board[rows][columns] = ' ';
+                Board[rowcounter][columncounter] = 'v';
+            }
+            else if (random < 55)
+            {
+                Board[rowcounter][columncounter] = '<';
+            }
+            else if (random < 60)
+            {
+                Board[rowcounter][columncounter] = '>';
+            }
+            else if (random < 80)
+            {
+                Board[rowcounter][columncounter] = 'r';
+            }
+            else if (random < 100)
+            {
+                Board[rowcounter][columncounter] = ' ';
             }
         }
     }
-}
-
-void printBoard(int row, int column)
-{
-    for (int rows = 0; rows < row; rows++)
+    Board[numofrows / 2][numofcolumns / 2] = 'A';
+    for (int zombiecounter = 0; zombiecounter < numofzombies; zombiecounter++)
     {
-        for (int columns = 0; columns < column; columns++)
+        randomrows = rand() % numofrows;
+        randomcolumns = rand() % numofcolumns;
+        if (Board[randomrows][randomcolumns] != 'A' || Board[randomrows][randomcolumns] != 'Z')
         {
-            if (rows % 2 == 0 && columns % 2 == 0)
-            {
-                cout << "+";
-            }
-            else if (rows % 2 == 0 && columns % 2 != 0)
-            {
-                cout << "-";
-            }
-            else if (columns % 2 == 0)
-            {
-                cout << "|";
-                if (columns == column - 1)
-                    cout << " ";
-            }
-            else if (columns % 2 != 0 && rows % 2 != 0)
-            {
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }
-}
-
-void quit()
-{
-    string ans;
-    cout << "Are you sure you want to quit? (Y/N) : ";
-    cin >> ans;
-    if (ans == "Y")
-    {
-        cout << "Thank you for playing" << endl;
-        std::terminate();
-    }
-    else if (ans == "N")
-    {
-    }
-}
-
-void userinput(char x)
-{
-    cout << "User input : ";
-    cin >> x;
-    x = toupper(x);
-    cout << endl;
-
-    switch (x)
-    {
-    case 'H':
-        help::main();
-        break;
-    case 'W':
-        cout << "Alien moves up" << endl;
-        // up movement
-        break;
-    case 'S':
-        cout << "Alien moves down" << endl;
-        // down
-        break;
-    case 'A':
-        cout << "Alien moves left" << endl;
-        // left
-        break;
-    case 'D':
-        cout << "Alien moves right" << endl;
-        // right
-        break;
-    case 'Z':
-        // save
-        break;
-    case 'X':
-        // load
-        break;
-    case 'Q':
-        quit();
-        break;
-    case 'R':
-        // rotate arrow
-        break;
-
-    default:
-        cout << "Invalid command, please refer to command list *H* for list of commands" << endl;
-        break;
-    }
-}
-
-/*void replace(int numofrows,int numofcolumns) //replace spaces in board with shitz
-{
-    string temp;
-    for (int x = 0; x < numofcolumns; x++)
-    {
-        for (int y = 0; y < numofrows; y++)
-        {
-            if (x % 2 != 0 && y % 2 != 0 )
-            {
-                temp = board[x][y];
-                board[x][y] = x;
-            }
+            Board[randomrows][randomcolumns] = 'Z';
         }
     }
-}*/
+
+    createBoard(numofrows, numofcolumns);
+}
+
+// void userinput(char x)
+// {
+//     cout << "User input : ";
+//     cin >> x;
+//     x = toupper(x);
+//     cout << endl;
+
+//     switch (x)
+//     {
+//     case 'Help':
+//         help::main();
+//         break;
+//     case 'Up':
+//         cout << "Alien moves up" << endl;
+//         // up movement
+//         break;
+//     case 'Down':
+//         cout << "Alien moves down" << endl;
+//         // down
+//         break;
+//     case 'Left':
+//         cout << "Alien moves left" << endl;
+//         // left
+//         break;
+//     case 'Right':
+//         cout << "Alien moves right" << endl;
+//         // right
+//         break;
+//     case 'SAve':
+//         // save
+//         break;
+//     case 'Load':
+//         // load
+//         break;
+//     case 'Quit':
+//         // quit
+//         break;
+//     case 'Arrow':
+//         // rotate arrow
+//         break;
+
+//     default:
+//         cout << "Invalid command, please refer to command list *H* for list of commands" << endl;
+//         break;
+//     }
+// }
 
 class Alien
 {
 public:
     int HP = 100;
-    int ATK = 0;
+    int ATK = 10;
 };
 
 class Zoms
 {
 public:
-    int HP = 200;
-    int ATK = 30;
+    int HP = 100 + rand() % 250;
+    int ATK = 20 + rand() % 40;
 };
 
 class Pods
@@ -242,27 +219,26 @@ class Healthpack
 public:
     int HP = 20; // adds hp to alien
 };
-
 int main()
 {
-    int numofrows;
-    int numofColumns;
-    int numofZombies;
-    char x;
-    bool win = false;
-    display_settingscreen(numofrows, numofColumns, numofZombies);
-    Createboard(numofrows, numofColumns);
-    printBoard(numofrows, numofColumns);
+    int numofrows = 3;
+    int numofColumns = 5;
+    int numofZombies = 1;
+    int selection;
 
+    char x;
+    display_settingscreen(numofrows, numofColumns, numofZombies);
+    feature(numofrows, numofColumns, numofZombies);
     // after spawning entities
-    if (win = false)
-    {
-        userinput(x);
-    }
-    else if (win = true)
-    {
-        cout << "You Win!";
-        pf::Pause();
-        std::terminate();
-    }
+    // userinput(x);
+//     if (win = false)
+//     {
+//         userinput(x);
+//     }
+//     else if (win = true)
+//     {
+//         cout << "You Win!";
+//         pf::Pause();
+//         std::terminate();
+//     }
 }
